e49bb3680b57a61560ce02775c7fb48d6f56d472
PhongVietCode <quocphogn1101@gmail.com>
2025-06-01 11:33:38 +0700

    fix: fix http client

--- src/task/Connect_Task.h
+++ src/task/Connect_Task.h
@@ -5,17 +5,16 @@
 #include "Wifi_Task.h"
 #include "Thingsboard_Task.h"
 
-extern Arduino_MQTT_Client mqttClient;
-// THINGSBOARD
-extern ThingsBoard tbClient;
-
 extern OTA_Firmware_Update<> ota;
 extern Server_Side_RPC<MAX_RPC_SUBSCRIPTIONS, MAX_RPC_RESPONSE> rpc;
 extern Shared_Attribute_Update<MAX_ATTRIBUTES_SUBSCRIPTIONS, MAX_ATTRIBUTES> shared_attributes;
 extern Attribute_Request<2U, MAX_ATTRIBUTES> attr_request;
-extern const std::array<IAPI_Implementation*, MAX_APIS> apis;
+extern const std::array<IAPI_Implementation *, MAX_APIS> apis;
+extern WiFiClient wifiClient;
+extern HTTPClient http;
+extern Arduino_MQTT_Client mqttClient;
+extern ThingsBoard tbClient;
 
 void connect_task_init();
-
 
 #endif
